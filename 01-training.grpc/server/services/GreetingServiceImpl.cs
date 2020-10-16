c77f67766c97b0927ce086fad82cd2075afc1d27
henkla <lthlarsson@gmail.com>
2020-10-16 07:45:38 +0200

    Implementing 6/27 - the streaming API proto and server side logic

--- 01-training.grpc/server/services/GreetingServiceImpl.cs
+++ 01-training.grpc/server/services/GreetingServiceImpl.cs
@@ -2,6 +2,7 @@
 using Grpc.Core;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Greet.GreetingService;
@@ -15,6 +16,18 @@ namespace server.services
             var result = $"Hello, {request.Greeting.FirstName} {request.Greeting.LastName}!";
 
             return Task.FromResult(new GreetingResponse { Result = result });
+        }
+
+        public override async Task GreetManyTimes(GreetManyTimesRequest request, IServerStreamWriter<GreetManyTimesResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine("The server recieved the request:");
+            Console.WriteLine(request.ToString());
+
+            var result = $"Hello, {request.Greeting.FirstName} {request.Greeting.LastName}!";
+            foreach (var index in Enumerable.Range(1, 10))
+            {
+                await responseStream.WriteAsync(new GreetManyTimesResponse() { Result = result });
+            }
         }
     }
 }
