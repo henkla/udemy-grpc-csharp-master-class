f5c33f4e2f9b53f4c0dfee55dbafdb36dbe1aa76
henkla <lthlarsson@gmail.com>
2020-10-19 10:00:41 +0200

    Implementing 9/50 - server reflection for testing with Evans

--- 01-training.grpc/server/Program.cs
+++ 01-training.grpc/server/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Greet;
 using Grpc.Core;
+using Grpc.Reflection;
+using Grpc.Reflection.V1Alpha;
 using server.services;
 using System;
 using System.Collections.Generic;
@@ -14,25 +16,23 @@ namespace server
 
         static void Main(string[] args)
         {
-            // get all the certificate files
-            var serverCert = File.ReadAllText("ssl/server.crt");
-            var serverKey = File.ReadAllText("ssl/server.key");
-            var caCert = File.ReadAllText("ssl/ca.crt");
+            // generate and configurate the ssl server credentials
+            //var serverCredentials = GenerateServerCredentials();
 
-            // create the server keypair
-            var keypair = new KeyCertificatePair(serverCert, serverKey);
-
-            // create the actual server credentials configuration providing the ssl details above
-            var serverCredentials = new SslServerCredentials(new List<KeyCertificatePair> { keypair }, caCert, true);
+            // creating a reflection service to register in the server
+            // this will allow for users to discover the api endpoints without
+            // having access to the .proto files in the first place
+            var reflectionServiceImpl = new ReflectionServiceImpl(GreetingService.Descriptor, ServerReflection.Descriptor);
 
             Server server = null;
-            
+
             try
             {
                 server = new Server()
                 {
-                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
-                    Ports = { new ServerPort(_host, _port, serverCredentials) }
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()),
+                                 ServerReflection.BindService(reflectionServiceImpl)},
+                    Ports = { new ServerPort(_host, _port, ServerCredentials.Insecure) }
                 };
 
                 server.Start();
@@ -48,6 +48,21 @@ namespace server
                 if (server != null)
                     server.ShutdownAsync().Wait();
             }
+        }
+
+        private static SslServerCredentials GenerateServerCredentials()
+        {
+            // get all the certificate files
+            var serverCert = File.ReadAllText("ssl/server.crt");
+            var serverKey = File.ReadAllText("ssl/server.key");
+            var caCert = File.ReadAllText("ssl/ca.crt");
+
+            // create the server keypair
+            var keypair = new KeyCertificatePair(serverCert, serverKey);
+
+            // create the actual server credentials configuration providing the ssl details above
+            var serverCredentials = new SslServerCredentials(new List<KeyCertificatePair> { keypair }, caCert, true);
+            return serverCredentials;
         }
     }
 }
